84fbf8bfa6f59d73f6061555bd4e6afdd6bfed72
cr9ck <andreyarosh29@gmail.com>
2019-07-25 12:47:19 +0300

    safe args added

--- app/src/main/java/com/cr9ck/navigationcomponent/HomeFragment.kt
+++ app/src/main/java/com/cr9ck/navigationcomponent/HomeFragment.kt
@@ -9,7 +9,7 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment () {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
@@ -17,26 +17,39 @@ class HomeFragment : Fragment () {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         initListrners()
-        val string: String? = arguments?.let { it.get("arg3") as String? }
-        val int: Int? = arguments?.let{ it.get("arg4") as Int? }
-        fragment_home_args.text = string + " = " + int
+        bundleFromProfile()
+        dataToSettings()
     }
 
     private fun initListrners() {
 
         fragment_home_next.setOnClickListener {
-            val navController: NavController? = activity?.let {
-                    it1 -> Navigation.findNavController(it1, R.id.nav_host_fragment)
+            val navController: NavController? = activity?.let { it1 ->
+                Navigation.findNavController(it1, R.id.nav_host_fragment)
             }
-            navController?.navigate(R.id.action_homeFragment_to_settingsFragment)
+            navController?.navigate(dataToSettings())
         }
         fragment_home_back.setOnClickListener {
-            val navController: NavController? = activity?.let {
-                    it1 -> Navigation.findNavController(it1, R.id.nav_host_fragment)
+            val navController: NavController? = activity?.let { it1 ->
+                Navigation.findNavController(it1, R.id.nav_host_fragment)
             }
             navController?.navigate(R.id.profileFragment)
         }
 
+    }
+
+    private fun bundleFromProfile() {
+        val string: String? = arguments?.let { it.get("arg3") as String? }
+        val int: Int? = arguments?.let { it.get("arg4") as Int? }
+        fragment_home_args.text = string + " = " + int
+    }
+
+    private fun dataToSettings(): HomeFragmentDirections.ActionHomeFragmentToSettingsFragment {
+        val action: HomeFragmentDirections.ActionHomeFragmentToSettingsFragment =
+            HomeFragmentDirections.actionHomeFragmentToSettingsFragment()
+        action.arg1 = 2
+        action.arg2 = "Hello"
+        return action
     }
 
 }
--- app/src/main/java/com/cr9ck/navigationcomponent/SettingsFragment.kt
+++ app/src/main/java/com/cr9ck/navigationcomponent/SettingsFragment.kt
@@ -9,18 +9,35 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_settings.*
 
-class SettingsFragment: Fragment() {
+class SettingsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        initListener()
+        getArgs()
+    }
+
+    private fun initListener() {
+
         fragment_settings_back.setOnClickListener {
-            val navController: NavController? = activity?.let {
-                    it1 -> Navigation.findNavController(it1, R.id.nav_host_fragment)
+            val navController: NavController? = activity?.let { it1 ->
+                Navigation.findNavController(it1, R.id.nav_host_fragment)
             }
             navController?.navigate(R.id.homeFragment)
         }
+
     }
+
+    private fun getArgs() {
+
+        val settingsFragmentArgs: SettingsFragmentArgs? = arguments?.let {
+            SettingsFragmentArgs.fromBundle(it)
+        }
+        fragment_settings_args.text = settingsFragmentArgs?.arg1.toString() + " = " + settingsFragmentArgs?.arg2
+
+    }
+
 }
